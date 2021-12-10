75769ea9215c261bcf4bff168d016d7f34244f07
inlolo <inlolo@gmail.com>
2021-12-11 01:05:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+#ssrplus+
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#passwall
+#echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
